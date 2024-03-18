a0791373affefa67e1de0796b45e6616630d6353
Joe Terlecki <joe.p.terlecki@gmail.com>
2024-03-18 19:06:45 -0400

    first commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    foo = TextNode("bar", "heading", "localhost")
+    print(foo)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,21 @@
+class TextNode:
+    def __init__(self, text, text_type, url) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, TextNode):
+            return NotImplemented
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self) -> str:
+        return (
+            f"TextNode({self.text.upper()}, "
+            f"{self.text_type.upper()}, "
+            f"{self.url.upper()})"
+        )
